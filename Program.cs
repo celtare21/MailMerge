cc40083cd29630acefbf5aa258704f45a80b85eb
Kuran Kaname <celtare21@gmail.com>
2020-06-14 20:21:20 +0300

    Update

--- Program.cs
+++ Program.cs
@@ -8,6 +8,10 @@ using System.IO;
 using System.Windows.Forms;
 using System.Data;
 using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Net;
+using System.Net.Mime;
 
 namespace MailMerge
 {
@@ -16,17 +20,33 @@ namespace MailMerge
         [STAThread]
         static void Main(string[] args)
         {
+            SmtpClient smtpClient;
             DataTable table;
             DocToPDFConverter converter;
             PdfDocument pdfDocument;
             Stream docStream = null;
             WordDocument template;
-            string folder;
+            List<string> emails;
+            string folder, email, pass;
+            int i = 0;
 
             SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
 
+            Console.WriteLine("Email:");
+            do
+            {
+                email = Console.ReadLine();
+            } while (email == "");
+
+            Console.WriteLine("Password:");
+            do
+            {
+                pass = Console.ReadLine();
+            } while (pass == "");
+
             table = new DataTable();
-            loadData(ref table);
+            emails = new List<string>();
+            loadData(ref table, ref emails);
 
             MessageBox.Show("Load Template Doc:");
             try
@@ -54,15 +74,41 @@ namespace MailMerge
                 Environment.Exit(0);
             }
 
+            smtpClient = new SmtpClient("smtp-mail.outlook.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(email, pass),
+                EnableSsl = true,
+            };
+
             foreach (DataRow dataRow in table.Rows)
             {
-                WordDocument document = template.Clone();
+                MailMessage mailMessage;
+                Attachment attachment;
+                WordDocument document;
+                string path;
 
+                document = template.Clone();
                 document.MailMerge.Execute(dataRow);
 
+                path = folder + "/Diploma_Logiscool_" + dataRow.ItemArray[0].ToString() + ".pdf";
+
                 pdfDocument = converter.ConvertToPDF(document);
-                pdfDocument.Save(folder + "/Diploma_Logiscool_" + dataRow.ItemArray[0].ToString() + ".pdf");
+                pdfDocument.Save(path);
                 pdfDocument.Close(true);
+
+                mailMessage = new MailMessage
+                {
+                    From = new MailAddress(email),
+                    Subject = "Test",
+                    Body = "Acesta este un test",
+                };
+
+                attachment = new Attachment(path, MediaTypeNames.Application.Pdf);
+                mailMessage.Attachments.Add(attachment);
+                mailMessage.To.Add(emails[i++]);
+
+                smtpClient.Send(mailMessage);
 
                 document.Dispose();
             }
@@ -70,10 +116,11 @@ namespace MailMerge
             MessageBox.Show("Done!");
         }
 
-        private static void loadData(ref DataTable elements)
+        private static void loadData(ref DataTable elements, ref List<string> emails)
         {
             ExcelWorksheet worksheet;
             ExcelFile loadedFile = null;
+            bool wrow = false;
 
             MessageBox.Show("Choose the database:");
             try
@@ -93,9 +140,15 @@ namespace MailMerge
             {
                 foreach (ExcelCell cell in row.AllocatedCells)
                 {
-                    if (String.Equals(cell.Value.ToString().ToUpper(), "NUME".ToString()))
+                    if (String.Equals(cell.Value.ToString().ToUpper(), "NUME".ToString()) || String.Equals(cell.Value.ToString().ToUpper(), "EMAIL".ToString()))
                         continue;
-                    elements.Rows.Add(cell.Value.ToString().ToUpper());
+
+                    if (!wrow)
+                        elements.Rows.Add(cell.Value.ToString().ToUpper());
+                    else
+                        emails.Add(cell.Value.ToString());
+
+                    wrow = !wrow;
                 }
             }
         }
