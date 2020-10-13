507d92658b818ab0691229db2bc0a274d93a8435
Kuran Kaname <celtare21@gmail.com>
2020-10-13 22:31:00 +0300

    Update

--- Program.cs
+++ Program.cs
@@ -1,37 +1,35 @@
-﻿using Syncfusion.DocIO;
-using Syncfusion.DocIO.DLS;
-using Syncfusion.DocToPDFConverter;
-using Syncfusion.Pdf;
-using GemBox.Spreadsheet;
-using System;
+﻿using System;
 using System.IO;
 using System.Windows.Forms;
 using System.Data;
-using Microsoft.WindowsAPICodePack.Dialogs;
 using System.Collections.Generic;
 using System.Net.Mail;
 using System.Net;
 using System.Net.Mime;
 using System.Security;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocToPDFConverter;
+using Syncfusion.Pdf;
+using GemBox.Spreadsheet;
 
 namespace MailMerge
 {
     class Program
     {
         [STAThread]
-        static void Main(string[] args)
+        static void Main()
         {
-            SmtpClient smtpClient;
-            DataTable names;
-            DocToPDFConverter converter;
-            PdfDocument pdfDocument;
-            Stream docStream = null;
-            StreamReader reader;
             WordDocument template, body;
+            SmtpClient smtpClient;
+            Stream docStream;
+            StreamReader reader;
+            DataTable names;
             List<string> emails;
-            string folder, email, mailBody;
+            string email, mailBody;
+            string folder, path = null, oldpath;
             SecureString pass;
-            string path = null, oldpath;
             int i = 0;
 
             SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
@@ -61,7 +59,7 @@ namespace MailMerge
             catch (ArgumentNullException)
             {
                 MessageBox.Show("No file loaded! Exiting!");
-                Environment.Exit(0);
+                return;
             }
             template = new WordDocument(docStream, FormatType.Docx);
             docStream.Dispose();
@@ -89,94 +87,99 @@ namespace MailMerge
             reader = new StreamReader(docStream);
             mailBody = reader.ReadToEnd();
             docStream.Dispose();
-            
-            converter = new DocToPDFConverter();
-            converter.Settings.EnableFastRendering = true;
-            converter.Settings.EmbedFonts = true;
-            converter.Settings.OptimizeIdenticalImages = true;
 
             MessageBox.Show("Choose output folder:");
             folder = selectFolder();
             if (folder == null)
             {
                 MessageBox.Show("No folder selected! Exiting!");
-                Environment.Exit(0);
+                return;
             }
 
-            smtpClient = new SmtpClient("smtp-mail.outlook.com")
+            Console.Clear();
+
+            using (smtpClient = new SmtpClient("smtp-mail.outlook.com")
             {
                 Port = 587,
                 Credentials = new NetworkCredential(email, pass),
                 EnableSsl = true,
-            };
-
-            Console.Clear();
-
-            foreach (DataRow dataRow in names.Rows)
+            })
             {
-                MailMessage mailMessage = null;
-                Attachment attachment;
-                WordDocument document;
-
-                oldpath = path;
-                path = folder + "\\Diploma_Logiscool_" + dataRow.ItemArray[0] + ".pdf";
-
-                if (path != oldpath)
+                foreach (DataRow dataRow in names.Rows)
                 {
-                    document = template.Clone();
-                    document.MailMerge.Execute(dataRow);
+                    MailMessage mailMessage;
 
-                    pdfDocument = converter.ConvertToPDF(document);
-                    pdfDocument.Save(path);
-                    pdfDocument.Close(true);
+                    oldpath = path;
+                    path = folder + "\\Diploma_Logiscool_" + dataRow.ItemArray[0] + ".pdf";
 
-                    document.Dispose();
-                }
-
-                try
-                {
-                    mailMessage = new MailMessage
+                    if (path != oldpath)
                     {
-                        From = new MailAddress(email),
-                        Subject = "Diploma Logiscool",
-                        Body = mailBody,
-                        IsBodyHtml = true,
-                    };
-                }
-                catch (FormatException)
-                {
-                    MessageBox.Show("Invalid email address!");
-                    Environment.Exit(0);
-                }
+                        using (WordDocument document = template.Clone())
+                        {
+                            document.MailMerge.Execute(dataRow);
 
-                Console.WriteLine(dataRow.ItemArray[0] + " " + emails[i]);
-                attachment = new Attachment(path, MediaTypeNames.Application.Pdf);
-                mailMessage.Attachments.Add(attachment);
-                try
-                {
-                    mailMessage.To.Add(emails[i]);
-                }
-                catch (FormatException)
-                {
-                    MessageBox.Show("Invalid email address in the tabel:" + emails[i]);
-                    Environment.Exit(0);
-                }
+                            using (DocToPDFConverter converter = new DocToPDFConverter())
+                            {
+                                converter.Settings.EnableFastRendering = true;
+                                converter.Settings.EmbedFonts = true;
+                                converter.Settings.OptimizeIdenticalImages = true;
+                                using (PdfDocument pdfDocument = converter.ConvertToPDF(document))
+                                {
+                                    pdfDocument.Save(path);
+                                    pdfDocument.Close(true);
+                                }
+                            }
+                        }
+                    }
 
-                System.Threading.Thread.Sleep(1500);
+                    try
+                    {
+                        mailMessage = new MailMessage
+                        {
+                            From = new MailAddress(email),
+                            Subject = "Diploma Logiscool",
+                            Body = mailBody,
+                            IsBodyHtml = true,
+                        };
+                    }
+                    catch (FormatException)
+                    {
+                        MessageBox.Show("Invalid email address!");
+                        return;
+                    }
 
-                try
-                {
-                    smtpClient.Send(mailMessage);
+                    Console.WriteLine(dataRow.ItemArray[0] + " " + emails[i]);
+                    using (Attachment attachment = new Attachment(path, MediaTypeNames.Application.Pdf))
+                    {
+                        mailMessage.Attachments.Add(attachment);
+
+                        try
+                        {
+                            mailMessage.To.Add(emails[i]);
+                        }
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("Invalid email address in the tabel:" + emails[i]);
+                            return;
+                        }
+
+                        try
+                        {
+                            smtpClient.Send(mailMessage);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Couldn't connect to the email address!" + ex.ToString());
+                            return;
+                        }
+
+                        mailMessage.Dispose();
+                    }
+
+                    Console.WriteLine("...Email sent!");
+
+                    ++i;
                 }
-                catch (SmtpException)
-                {
-                    MessageBox.Show("Couldn't connect to the email address!");
-                    Environment.Exit(0);
-                }
-
-                Console.WriteLine("...Email sent!");
-
-                ++i;
             }
 
             MessageBox.Show("Done!");
@@ -214,7 +217,7 @@ namespace MailMerge
         private static void loadData(ref DataTable names, ref List<string> emails)
         {
             ExcelWorksheet worksheet;
-            ExcelFile loadedFile = null;
+            ExcelFile loadedFile;
             string scell;
             bool wrow = false;
 
@@ -230,6 +233,7 @@ namespace MailMerge
                 else if (ex is FreeLimitReachedException)
                     MessageBox.Show("More than 150 rows loaded! Exiting!");
                 Environment.Exit(0);
+                return;
             }
 
             names.Columns.Add("NUME", typeof(string));
@@ -273,13 +277,14 @@ namespace MailMerge
 
         static private string loadFile(bool doc)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-
-            openFileDialog.CheckFileExists = true;
-            openFileDialog.CheckPathExists = true;
-            openFileDialog.RestoreDirectory = true;
-            openFileDialog.FilterIndex = 2;
-            openFileDialog.ShowReadOnly = true;
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                CheckFileExists = true,
+                CheckPathExists = true,
+                RestoreDirectory = true,
+                FilterIndex = 2,
+                ShowReadOnly = true
+            };
 
             if (doc)
             {
